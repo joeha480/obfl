0c6c474ba63927830947b7994e8fcfbbda24661f
Joel Håkansson <joel.hakansson@mtm.se>
2018-01-17 14:17:47 +0100

    Publish all branches on travis (fixes #57)

--- build-resources/publish.sh
+++ build-resources/publish.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "braillespecs/obfl" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-
+if [ "$TRAVIS_REPO_SLUG" == "braillespecs/obfl" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Publishing...\n"
+
+  publishTo="obfl/${TRAVIS_BRANCH//'/'/_}"
 
   cp -R build/site $HOME
 
@@ -12,12 +13,13 @@ if [ "$TRAVIS_REPO_SLUG" == "braillespecs/obfl" ] && [ "$TRAVIS_PULL_REQUEST" ==
   git clone --quiet https://${GH_TOKEN}@github.com/braillespecs/braillespecs.github.io  > /dev/null
 
   cd braillespecs.github.io
-  git rm -rf ./obfl
-  cp -Rf $HOME/site ./obfl
+  mkdir -p ./${publishTo}
+  git rm -rf ./${publishTo}
+  cp -Rf ${HOME}/site ./${publishTo}
   git add -f .
   git commit -m "Travis build of obfl ($TRAVIS_BUILD_NUMBER) auto-pushed"
   git push -fq origin master > /dev/null
 
-  echo -e "Published site to braillespecs.github.io/obfl.\n"
+  echo -e "Published site to braillespecs.github.io/$publishTo.\n"
   
 fi
